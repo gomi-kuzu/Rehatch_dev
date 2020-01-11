ab6f009e762b7ef90b5e839bca6bbb4c96d0c8eb
nozutaku <nozutaku@gmail.com>
2020-01-12 00:57:32 +0900

    LINE-bot

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 import os
 import slack
 from urllib import request as req
@@ -6,11 +6,33 @@ import random
 import xmltodict
 import urllib.parse
 import requests
+import json
+import urllib.request
 
 app = Flask(__name__)
 
 client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
 root_url = "https://crd.ndl.go.jp/api/refsearch"
+
+##### LINE bot SETTING(START)
+from linebot import (
+    LineBotApi, WebhookParser
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+line_channel_access_token = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+line_channel_secret = os.environ["LINE_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(line_channel_access_token)
+# handler = WebhookHandler(LINE_CHANNEL_SECRET)
+parser = WebhookParser(line_channel_secret)
+
+##### LINE bot SETTING(END)
 
 
 # def get_matchs(query, db):
@@ -85,7 +107,35 @@ def make_response(url,name):
 def send_to_slack(send_text,channel_name= "#botデバッグ用"):
   response = client.chat_postMessage(channel=channel_name, text=send_text,icon_emoji = ":rehatch_1:",username="れはっち" )
 
-
+def record_log_to_kintone( source, send_text, sender ):
+  
+  url = os.environ['KINTONE_URL']
+  headers = {
+    "X-Cybozu-API-Token": os.environ['CYBOZU_LOG_DB_API_TOKEN'],
+    "Content-type": "application/json",
+  }
+  data = {
+    "app": os.environ['CYBOZU_LOG_DB_APP_ID'],
+    "record": {
+      "type": {
+        "value": source
+      },
+      "questioner": {
+        "value": sender
+        },
+      "log": {
+        "value": send_text
+      },
+    }
+  }
+  req = urllib.request.Request(url, json.dumps(data).encode(), headers)
+  with urllib.request.urlopen(req) as res:
+    body = res.read()
+    # print("body=\n" + body.hex())
+  
+  
+  
+  
 @app.route('/')
 def hello():
 
@@ -130,6 +180,71 @@ def recieve_post_slack():
     send_to_slack(''.join(reps))
     
   return ''
+
+
+######## LINE bot (START) ########
+# https://github.com/line/line-bot-sdk-python/tree/master/examples/flask-echo
+
+@app.route("/line_callback", methods=['POST'])
+def callback():
+  # get X-Line-Signature header value
+  signature = request.headers['X-Line-Signature']
+  
+  print( "start line callback" )
+
+  # get request body as text
+  body = request.get_data(as_text=True)
+
+
+  # parse webhook body
+  try:
+    print("Request body: " + body)
+    events = parser.parse(body, signature)
+  except InvalidSignatureError:
+    abort(400)
+
+  # if event is MessageEvent and message is TextMessage, then get text
+  for event in events:
+    if not isinstance(event, MessageEvent):
+      continue
+    if not isinstance(event.message, TextMessage):
+      continue
+
+    if event.reply_token == "00000000000000000000000000000000" or event.reply_token == "ffffffffffffffffffffffffffffffff":
+      print( " from system message. so do not to do.\n ")
+      return 'OK'
+    
+    #reply LINE bot
+    query = event.message.text
+    url = make_url(query)
+    reps,link_url = make_response(url,query)
+    for r in reps:
+      print('> {}'.format(r))
+  
+    if link_url:
+      reply_message = ''.join(reps) + "\n" + link_url
+    else:
+      reply_message = ''.join(reps)
+      
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage( text=reply_message )
+      )
+    
+    record_log_to_kintone( "LINE_BOT", event.message.text, event.source.user_id )     #event.source.userIdで無い理由は不明
+    
+    
+    #line_bot_api.reply_message(
+    #  event.reply_token,
+    #  TextSendMessage(text=event.message.text)
+    #)
+    print("line reply end.")
+
+  return 'OK'
+  
+
+######## LINE bot (END) ########
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
